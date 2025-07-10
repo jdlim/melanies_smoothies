b6c37cd2354417563040b1e54eb18ae0317ecde4
John Lim <jdlimusa@gmail.com>
2025-07-10 11:20:03 -0400

    Update streamlit_app.py
    Fixing an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_repsonse.json(), use_container_width=true)
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
